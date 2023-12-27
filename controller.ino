5e8acf8cfbab930a83ce371c4680026d5e90b1bb
TylerS <tylersnell@me.com>
2023-12-26 22:18:59 -0800

    Update controller.ino

--- controller.ino
+++ controller.ino
@@ -1,404 +1,236 @@
 #include <ArduinoTapTempo.h>
-#include <Adafruit_seesaw.h>
 #include <Bounce2.h>
-#include <Adafruit_SSD1351.h>
-#include <Adafruit_GFX.h>
-#include <ArduinoBLE.h>
 
-// OLED Display Configuration
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 128
-#define OLED_RESET -1
+// TODO:
 
-// You can use any (4 or) 5 pins
-#define SCLK_PIN 13
-#define MOSI_PIN 11
-#define DC_PIN 8
-#define CS_PIN 10
-#define RST_PIN 9
-
-// Create an instance of the Adafruit_SSD1351 display object
-Adafruit_SSD1351 tft = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, CS_PIN, DC_PIN, RST_PIN);
-
-#define BLACK 0x0000
-#define BLUE 0x001F
-#define RED 0xF800
-#define DARK_RED 0x8B0000
-#define GREEN 0x07E0
-#define CYAN 0x07FF
-#define MAGENTA 0xF81F
-#define YELLOW 0xFFE0
-#define WHITE 0xFFFF
-
-const int BUTTON_PIN = 2;
-const int ENCODER_INTERRUPT_PIN = 24;
-const int RESET_BUTTON_PIN = 5;
-const int LED_PIN = 3;  // Pin for the LED
+const int BUTTON_PIN = 9;
+const int ALT_MODE_BUTTON_PIN = 11;
+const int LED_PIN = A5;  // Pin for the LED
 const int SERIAL_BAUD_RATE = 9600;
 ArduinoTapTempo tapTempo;
-Adafruit_seesaw ss;
 
-int32_t encoder_position;
-int ENCODER_MODE = false;
+const int ledPins[] = { 13, A1, A2, A3, A4 };  // Define an array of button pins
+const int numLEDs = 5;                         // Number of LEDs
+const int buttonPins[] = { 4, 2, 3, 7, 8 };    // Define an array of button pins
+const int numButtons = 5;
+int buttonChar[numButtons];
+
 int bpmAdjust = 0;
 int currentBPM = 120;  // Store the current BPM
+int adjustedBPM = 120;
+int persistantBPM = 120;
+int maxBPM = 250;
+int minBPM = 10;
 bool resetDownbeat = false;
 unsigned long resetTime = 0;
+int currentButton = 9;
+int brightness = 255;
+int color = 1;
+int animation = 0;
 
 unsigned long lastUpdateMillis = 0;
 const unsigned long updateInterval = 10;
 
-Bounce resetButton = Bounce();
+unsigned long ledBlinkDuration[5] = { 0 };
 
+unsigned long previousMillis = 0;
+unsigned long interval;
+
+Bounce altModeButton = Bounce();
+Bounce buttons[5];  // Create an array of debounced buttons
+
+bool ledState[5] = { false };     // Store the state of each LED
 bool resetButtonPressed = false;  // Flag to track reset button press
 
-// Define menu headers
-static const char* menuHeaders[] = {
-  "_touch:",
-  "Animate",
-  "Pattern",
-  "Devices"
-};
+unsigned long previousTimer = 0;       // Initialize a variable to store the last time the code ran
+const unsigned long timerInter = 500;  // Interval in milliseconds (1 second)
 
-// Define menu states
-enum MenuState {
-  MAIN_MENU,
-  ANIMATE,
-  NUMBERS,
-  DEVICES
-};
-
-// Variables to track the current menu state and the selected menu item
-MenuState currentMenu = MAIN_MENU;
-int selectedMenuItem = 1;       // Start with the first menu item selected
-bool menuItemSelected = false;  // Flag to track menu item selection
-
-bool screenNeedsUpdate = true;
+// Global variables to track the last sent values
+int lastSentBPM = 0;          // Initialize with an impossible BPM value
+int lastSentButtonState = 9;  // Initialize with an impossible button state
 
 void setup() {
-  Serial.begin(SERIAL_BAUD_RATE);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
 
-  if (!ss.begin(0x36)) {
-    Serial.println("Couldn't find seesaw on default address");
-    while (1) delay(10);
-  }
+  Serial.begin(9600);  // initialize serial communication at 9600 bits per second:
+  Serial1.begin(9600);
 
   tapTempo.setTotalTapValues(4);
-  tapTempo.setBeatsUntilChainReset(20);
+  tapTempo.setBeatsUntilChainReset(8);
   tapTempo.setMaxBPM(200);
-  tapTempo.setMinBPM(40);
+  tapTempo.setMinBPM(50);
 
-  BLE.begin();  // initialize the Bluetooth® Low Energy hardware
+  for (int i = 0; i < 5; i++) {
+    pinMode(buttonPins[i], INPUT_PULLUP);  // Set button pins as INPUT_PULLUP
+    pinMode(ledPins[i], OUTPUT);           // Set LED pins as OUTPUT
+    buttons[i].attach(buttonPins[i]);
+    buttons[i].interval(50);  // Debounce interval in milliseconds
+  }
 
-  Serial.println("Bluetooth® Low Energy Central - LED control");
-
-  BLE.scanForName("Invisible Touch");  // start pre scanning for peripherals
-
-  ss.enableEncoderInterrupt(ENCODER_INTERRUPT_PIN);
-
-  encoder_position = ss.getEncoderPosition();
-
-  pinMode(RESET_BUTTON_PIN, INPUT_PULLUP);
-  resetButton.attach(RESET_BUTTON_PIN);
-  resetButton.interval(50);  // Debounce interval for the reset button
-
-  pinMode(LED_PIN, OUTPUT);  // Set LED pin as output
-
-  // Initialize the OLED display
-  tft.begin();  // Initialize the OLED display using the tft object
-  tft.setRotation(1);
-  tft.fillScreen(BLACK);  // Fill the screen with black
+  pinMode(ALT_MODE_BUTTON_PIN, INPUT_PULLUP);
+  altModeButton.attach(ALT_MODE_BUTTON_PIN);
+  altModeButton.interval(50);
 
   delay(1000);
-  tft.fillScreen(BLACK);  // Clear the display
 }
-
+bool buttonPressedLast[5] = { false };
+bool lastAltMode = false;
 void loop() {
-  handleMenuNavigation();
+  unsigned long currentTimer = millis();  // Get the current time
+  altModeButton.update();                 // Update the alternate mode button state
 
-  // Check if a menu item has been selected
-  if (menuItemSelected) {
-    switch (selectedMenuItem) {
-      case 1:
-        currentMenu = ANIMATE;
-        break;
-      case 2:
-        currentMenu = NUMBERS;
-        break;
-      case 3:
-        currentMenu = DEVICES;
-        break;
+  bool altMode = altModeButton.read() == HIGH;  // Check if the alternate mode is active
+
+  for (int i = 0; i < 5; i++) {
+    buttons[i].update();  // Update the button states
+
+    bool buttonPressed = buttons[i].read() == LOW;
+    if (i == 3 && buttonPressed && !buttonPressedLast[i]) {
+      currentBPM = currentBPM + currentBPM;  // Increase BPM by 50%
+      if (currentBPM > maxBPM) {
+        currentBPM = maxBPM;  // Cap at maximum BPM
+      }
+    } else if (i == 4 && buttonPressed && !buttonPressedLast[i]) {
+      currentBPM = currentBPM / 2;  // Decrease BPM by 50%
+      if (currentBPM < minBPM) {
+        currentBPM = minBPM;  // Cap at minimum BPM
+      }
+    } else if (altMode && i == 0 && buttonPressed && !buttonPressedLast[i]) {
+      adjustedBPM++;  // Increase BPM
+    } else if (altMode && i == 1 && buttonPressed && !buttonPressedLast[i]) {
+      adjustedBPM--;  // Decrease BPM
+    } else if (altMode && i == 2 && buttonPressed && !buttonPressedLast[i]) {
+      brightness = (brightness + 10) % 265;  // Increase brightness by 10 each press, wrap around at 255
+    } else if (altMode && i == 3 && buttonPressed && !buttonPressedLast[i]) {
+      color = 3 - color;  // Toggle color between 1 and 2
+    } else if (buttonPressed && !buttonPressedLast[i]) {
+      ledState[i] = !ledState[i];                // Toggle LED state
+      digitalWrite(ledPins[i], HIGH);            // Turn on the LED
+      ledBlinkDuration[i] = currentTimer + 150;  // Set the time to turn off the LED (adjustable duration)
+      currentButton = i;
     }
 
-    // Reset the flag
-    menuItemSelected = false;
+    buttonPressedLast[i] = buttonPressed;
+
+    // Check if it's time to turn off the LED
+    if (currentTimer >= ledBlinkDuration[i] && digitalRead(ledPins[i]) == HIGH) {
+      digitalWrite(ledPins[i], LOW);  // Turn off the LED
+    }
   }
 
   bool buttonDown = digitalRead(BUTTON_PIN) == LOW;
   tapTempo.update(buttonDown);
-
-  resetDownbeatOnButtonPress();
-
-  if (resetDownbeat) {
-    // Restore the BPM to the stored currentBPM value
-    // bpmAdjust = currentBPM - tapTempo.getBPM();
-    resetDownbeat = false;
-    ENCODER_MODE = true;
-  }
-
-  currentBPM = tapTempo.getBPM();
-
-  ENCODER_MODE = false;
-  int adjustedBPM = currentBPM + bpmAdjust;
- Serial.println(adjustedBPM);
-  if (adjustedBPM < 20) {
-    adjustedBPM = 1;
-  } else if (adjustedBPM > 300) {
-    adjustedBPM = 300;
-  }
-
-  displayMenu(adjustedBPM);
-  controllerBpmLed(adjustedBPM);
-}
-
-//========================================================================================================= Menu ================= Menu
-void displayMenuHeader() {
-  tft.setTextColor(WHITE, BLACK);  // Set text color
-
-  // Display the selected menu header
-  tft.setTextSize(2);
-  tft.setCursor(10, 10);
-  tft.println(menuHeaders[currentMenu]);
-}
-
-void displayMenuItem(int itemIndex, bool selected) {
-  tft.setTextSize(1);
-  tft.setTextColor(selected ? RED : WHITE);  // Highlight the selected item
-  tft.setCursor(10, 40 + (itemIndex * 20));
-  tft.println(menuHeaders[itemIndex]);
-}
-
-void displayMenu(int bpm) {
-  displayMenuHeader();
-
-  bigBPM(bpm, bpmAdjust);
-
-  for (int i = 1; i <= 3; i++) {
-    displayMenuItem(i, i == selectedMenuItem);
-  }
-}
-
-void handleMenuNavigation() {
-  int encoderValue = ss.getEncoderPosition();
-  static int lastEncoderValue = encoderValue;
-
-  if (resetButtonPressed) {
-     adjustBPM();
-  }
-
-
-  if (encoderValue != lastEncoderValue && !resetButtonPressed) {
-    // Rotary knob rotated, update the selected menu item
-    selectedMenuItem += (encoderValue - lastEncoderValue);
-
-    // Ensure the selectedMenuItem stays within bounds
-    if (selectedMenuItem < 1) {
-      selectedMenuItem = 1;
-    } else if (selectedMenuItem > 3) {
-      selectedMenuItem = 3;
-    }
-
-    lastEncoderValue = encoderValue;
-  }
-
-  // Handle menu item confirmation with the rotary push button
-  if (!ss.digitalRead(ENCODER_INTERRUPT_PIN)) {
-    menuItemSelected = true;
-  }
-}
-
-void animateMenuItemSelected() {
-  connectToLed();
-  // Code to perform when "Animate" menu item is selected
-  // Add your animation logic here
-}
-
-void numbersMenuItemSelected() {
-  // Code to perform when "Numbers" menu item is selected
-  // Add your numbers-related logic here
-}
-
-void devicesMenuItemSelected() {
-  // Code to perform when "Devices" menu item is selected
-  // Add your device-related logic here
-}
-
-void bigBPM(int bpm, int nudge) {
-  tft.setTextColor(RED, BLACK);
-  tft.setCursor(10, 10);
-  // tft.print("BPM:");
-  tft.setTextSize(3);
-  tft.setCursor(70, 57);
-  tft.print(bpm);
-  tft.setTextSize(2);
-  tft.setTextColor(WHITE, BLACK);
-  tft.setCursor(70, 93);
-  tft.print(nudge);
-  tft.print("  ");
-}
-
-// ========================================================================== Physical Hardware Items ============= Physical Hardware Items
-void controllerBpmLed(int adjustedBPM){
-  // Serial.println(adjustedBPM);
-  unsigned long currentMillis = millis();
-  if (currentMillis - lastUpdateMillis >= updateInterval) {
-    // Serial.print("bpm: ");
-    // Serial.println(currentBPM);
-
-    // Calculate the LED blink interval based on the adjusted BPM
-    int ledBlinkInterval = 60000 / adjustedBPM;
-
-    // Blink the LED
-    static unsigned long previousMillis = 0;
-
-    if (currentMillis - previousMillis >= ledBlinkInterval / 2 && !resetButtonPressed) {
-      digitalWrite(LED_PIN, HIGH);
-    } else {
-      
-    }
-
-    if (currentMillis - previousMillis >= ledBlinkInterval && !resetButtonPressed) {
-      digitalWrite(LED_PIN, LOW);
-      previousMillis = currentMillis;
-    }
-
-    lastUpdateMillis = currentMillis;
-  }
-}
-
-// ==================================================================================== BPM Manipulation ================= BPM Manipulation
-
-void adjustBPM() {
-  int32_t new_position = ss.getEncoderPosition();
-  if (encoder_position != new_position) {
-    // Calculate the encoder movement direction
-    int direction = new_position > encoder_position ? -1 : 1;  // Inverted direction for clockwise
-
-    // "Nudge" the BPM higher or lower
-    int nudgeAmount = 1;  // Adjust this value to control the nudge amount
-
-    // Update the BPM adjustment based on direction and nudge amount
-    bpmAdjust += direction * nudgeAmount;
-
-    // Limit the BPM adjustment to a reasonable range
-    if (bpmAdjust < -100) {
-      bpmAdjust --;
-    } else if (bpmAdjust > 100) {
-      bpmAdjust ++;
-    }
-
-    encoder_position = new_position;
-  }
-}
-
-void resetDownbeatOnButtonPress() {
-  resetButton.update();
-
-  if (resetButton.rose()) {     // Actuate on button release
-    resetButtonPressed = true;  // Set the flag to indicate the button press
-  }
-
-  if (resetButton.fell()) {
-    digitalWrite(LED_PIN, LOW);  // Turn off the LED
-  }
-
-  if (resetButton.fell() && resetButtonPressed) {  // Button released after a press
-    // Check if the resetDownbeat flag was not set recently (prevent repeated reset)
-    if (!resetDownbeat && millis() - resetTime > 1000) {
+  if (!altMode) {
+    if (tapTempo.isChainActive()) {
       currentBPM = tapTempo.getBPM();
-      resetDownbeat = true;
-      resetTime = millis();
-      resetTapTempo();  // Reset the tap tempo
     }
-
-    resetButtonPressed = false;  // Reset the button press flag
-  }
-}
-
-void resetTapTempo() {
-  // Reset the tap tempo by simulating a new tap
-  tapTempo.update(true);   // Simulate a button press (a tap)
-  tapTempo.update(false);  // Release the button
-  Serial.println("Downbeat reset.");
-}
-
-// ========================================================================================= Bluetooth ========================= Bluetooth
-void connectToLed() {
-  BLEDevice peripheral = BLE.available();
-  if (peripheral) {
-    // discovered a peripheral, print out address, local name, and advertised service
-    Serial.print("Found ");
-    Serial.print(peripheral.address());
-    Serial.print(" '");
-    Serial.print(peripheral.localName());
-    Serial.print("' ");
-    Serial.print(peripheral.advertisedServiceUuid());
-    Serial.println();
-
-    if (peripheral.localName() != "Invisible Touch") {
-      return;
+  } else {
+    if (!lastAltMode) {
+      adjustedBPM = currentBPM;  // Update adjustedBPM with currentBPM when entering altMode
     }
-   controlLed(peripheral);
-    // stop scanning
-    BLE.stopScan();
+    currentBPM = adjustedBPM;  // Use adjustedBPM as currentBPM when in altMode
+  }
+  lastAltMode = altMode;
 
-    // peripheral disconnected, start scanning again
-    // BLE.scanForUuid("3497EEC9-1282-21EB-4641-A65A3F740D8E");
-    BLE.scanForName("Invisible Touch");
+  transmitter(currentBPM, currentButton, brightness, color, animation);
+
+  unsigned long currentMillis = millis();
+
+  interval = 60000 / currentBPM;  // Use currentBPM instead of adjustedBPM
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));  // Toggle the LED state
+  }
+
+  if (currentTimer - previousTimer >= timerInter) {
+    previousTimer = currentTimer;  // Update the previous time
+    Serial.println(currentBPM);    // Print currentBPM instead of adjustedBPM
   }
 }
 
-void controlLed(BLEDevice peripheral) {
-  // connect to the peripheral
-  Serial.println("Connecting...");
-  if (peripheral.connect()) {
-    Serial.println("Connected");
-  } else {
-    Serial.println("Failed to connect!");
-    return;
+void transmitter(int bpm, int buttonState, int brightness, int color, int animation) {
+  static int lastBPM = -1;
+  static int lastButtonIndex = -1;
+  static int lastBrightness = -1;
+  static int lastColor = -1;
+  static int lastAnimation = -1;
+
+  if (!tapTempo.isChainActive()) {
+    // Send BPM if it has changed
+    if (bpm != lastBPM && bpm >= 30 && bpm <= 250) {
+      uint8_t header = bpm < 100 ? 0x03 : 0x01;  // Use a different header for two-digit BPM
+      uint8_t bpmData[] = {
+        header,                                   // Header for BPM
+        static_cast<uint8_t>((bpm >> 8) & 0xFF),  // High byte of BPM
+        static_cast<uint8_t>(bpm & 0xFF)          // Low byte of BPM
+      };
+      Serial1.write(bpmData, sizeof(bpmData));
+      lastBPM = bpm;
+
+      // Print bpmData and sizeof(bpmData)
+      Serial.print("bpmData: ");
+      for (int i = 0; i < sizeof(bpmData); i++) {
+        Serial.print(bpmData[i], HEX);
+        Serial.print(" ");
+      }
+      Serial.println();
+
+      Serial.print("sizeof(bpmData): ");
+      Serial.println(sizeof(bpmData));
+    }
   }
 
-  // discover peripheral attributes
-  Serial.println("Discovering attributes ...");
-  if (peripheral.discoverAttributes()) {
-    Serial.println("Attributes discovered");
-    Serial.println(peripheral.discoverAttributes());
-  } else {
-    Serial.println("Attribute discovery failed!");
-    peripheral.disconnect();
-    return;
+  // Send Button State if it has changed
+  if (currentButton != lastButtonIndex && currentButton >= 0 && currentButton <= 5) {
+    uint8_t buttonData[] = {
+      0x02,                                // Header for Button State
+      static_cast<uint8_t>(currentButton)  // Button state
+    };
+    Serial1.write(buttonData, sizeof(buttonData));
+    lastButtonIndex = currentButton;
+
+    Serial.print("Button state sent: ");
+    Serial.println(currentButton);
   }
 
-  // retrieve the LED characteristic
-  BLECharacteristic ledCharacteristic = peripheral.characteristic("19b10000-e8f2-537e-4f6c-d104768a1214");
+  // Send Brightness if it has changed
+  if (brightness != lastBrightness && brightness >= 0 && brightness <= 255) {
+    uint8_t brightnessData[] = {
+      0x04,                             // Header for Brightness
+      static_cast<uint8_t>(brightness)  // Brightness value
+    };
+    Serial1.write(brightnessData, sizeof(brightnessData));
+    lastBrightness = brightness;
 
-  if (!ledCharacteristic) {
-    Serial.println("Peripheral does not have LED characteristic!");
-    peripheral.disconnect();
-    return;
-  } else if (!ledCharacteristic.canWrite()) {
-    Serial.println("Peripheral does not have a writable LED characteristic!");
-    peripheral.disconnect();
-    return;
+    Serial.print("Brightness sent: ");
+    Serial.println(brightness);
   }
 
-  while (peripheral.connected()) {
-    // while the peripheral is connected
+  // Send Color if it has changed
+  if (color != lastColor && (color == 1 || color == 2)) {
+    uint8_t colorData[] = {
+      0x05,                        // Header for Color
+      static_cast<uint8_t>(color)  // Color value
+    };
+    Serial1.write(colorData, sizeof(colorData));
+    lastColor = color;
 
-    // button is released, write 0x00 to turn the LED off
-    ledCharacteristic.writeValue((byte)0x00);
+    Serial.print("Color sent: ");
+    Serial.println(color);
   }
 
-  Serial.println("Peripheral disconnected");
+  // Send Animation if it has changed
+  if (animation != lastAnimation && (animation == 0 || animation == 1 || animation == 2)) {
+    uint8_t animationData[] = {
+      0x06,                            // Header for Animation
+      static_cast<uint8_t>(animation)  // Animation value
+    };
+    Serial1.write(animationData, sizeof(animationData));
+    lastAnimation = animation;
+
+    Serial.print("Animation sent: ");
+    Serial.println(animation);
+  }
 }
